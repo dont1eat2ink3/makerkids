8f66c677a423b239c362ad43704053fab27704d5
dont1eat2ink3 <ameera.behrainwala@outlook.com>
2024-09-21 18:56:17 +0000

    end of class sept 21

--- csv/video_games/main.py
+++ csv/video_games/main.py
@@ -1,55 +1,118 @@
-file = open ("videogames.csv","r")
+file = open("videogames.csv", "r")
+
+
 def display_games():
-    for line in file :
+    for line in file:
         data = line.split(",")
-        
+
+
         title = data[0]
         release_year = data[1]
         genre = data[2]
         developer = data[3]
         platform = data[4]
-
+       
         print("Title: " + title)
-        print("Release year: " + release_year)
+        print("Release Year: " + release_year)
         print("Genre: " + genre)
         print("Developer: " + developer)
         print("Platform: " + platform)
-display_games()
+        print()
+
+
+
+
 def list_games_by_year():
-    year = input("What year are you the most interested in?")
-    for line in file :
+    year = input("What year are you interested in? ")
+
+    with open("videogames.csv","r") as file:
+        next file
+
+
+    for line in file:
         data = line.split(",")
-        
+
+
         title = data[0]
         release_year = data[1]
         genre = data[2]
         developer = data[3]
         platform = data[4]
 
-        print("Title: " + title)
-        print("Release year: " + release_year)
-        print("Genre: " + genre)
-        print("Developer: " + developer)
-        print("Platform: " + platform)
-list_games_by_year()
+
+        if int(release_year) <=user_year:
+            print("Title: " + title)
+            print("Release Year: " + release_year)
+            print("Genre: " + genre)
+            print("Developer: " + developer)
+            print("Platform: " + platform)
+            print()
 
 
 
-def developer():
-    dev = input("What developer are you the most interested in?")
-    for line in file :
+
+def list_games_by_developer():
+    dev = input("Which Developer are you interested in? ")
+
+
+    for line in file:
         data = line.split(",")
-        
+
+
         title = data[0]
         release_year = data[1]
         genre = data[2]
         developer = data[3]
         platform = data[4]
 
-        print("Title: " + title)
-        print("Release year: " + release_year)
-        print("Genre: " + genre)
-        print("Developer: " + developer)
-        print("Platform: " + platform)
-developer()
+
+        if dev.lower() == developer.lower()
+            print("Title: " + title)
+            print("Release Year: " + release_year)
+            print("Genre: " + genre)
+            print("Developer: " + developer)
+            print("Platform: " + platform)
+            print()
+
+def = input("what genre are you interested in?")
+
+for line in file ():
+    data = line.split(",")
+    title = data[0]
+    release_year = data[1]
+    genre = data[2]
+    developer = data[3]
+    platform = data[4]
+
+    if user_genre.lower() == genre lower()
+            print("Title: " + title)
+            print("Release Year: " + release_year)
+            print("Genre: " + genre)
+            print("Developer: " + developer)
+            print("Platform: " + platform)
+            print()
+
+def list_games_by_platforms():
+    user_platform = imput("Enter a platform:")
+
+    for line in file:
+        data = line.split(",")
+
+        title = data[0]
+        release_year =data[1]
+        genre = data[2]
+        developer = data[3]
+        platform = data[4]
+
+        if user platform.lower() in platform.lower():
+            print("Title:" + title)
+            print("Release year:" + release_year)
+            print("Genre:" + genre)
+            print("Developer:" + developer)
+            print("platform:" + platform)
+
+#display_games()
+#list_games_by_year()
+list_games_by_developer()
+
 
